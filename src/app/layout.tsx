3f6c5166985f4d20e7c95f84382cae5774a79c0f
Aifia Mohamed Amine <aifiamohamedamine@isamm.u-manouba.tn>
2024-10-22 10:52:10 +0100

    remove font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "./providers/providers";
 import NavBarComponent from "@/components/navBarComponent";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Pokemon Analytics",
@@ -29,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <Providers>
           <NavBarComponent></NavBarComponent>
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { Button } from "@nextui-org/react";
-import ThemeSwitcher from "../components/themeSwitcher";
-import NavBarComponent from "@/components/navBarComponent";
-
 export default function Home() {
   return <div></div>;
 }
